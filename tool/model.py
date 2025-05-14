9fbbbea70757210332c5cfadaa9205bd19b9ba88
leejeongho3214 <wowo933@naver.com>
2025-05-15 01:36:29 +0900

    250514 basic ver.

--- tool/model.py
+++ tool/model.py
@@ -118,7 +118,7 @@ class Model(object):
 
     def print_loss(self, dataloader_len, final_flag=False):
         print(
-            f"\r[{self.args.git_name}] Epoch: {self.epoch} [{self.phase}][{self.m_dig}][{self.iter * (self.iter_epoch + 1) + self.iter}/{self.rep * dataloader_len}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}",
+            f"\r[{self.args.git_name}] Epoch: {self.epoch} [{self.phase}][{self.m_dig}][{self.iter}/{dataloader_len}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}",
             end="",
         )
         loss_phase, loss_avg = (
@@ -171,12 +171,12 @@ class Model(object):
                         correct_[i] += 1
 
                 info_m = (
-                    f"[Lr: {self.optimizer.param_groups[0]['lr']:4f}][Gamma: {self.args.gamma}][Early Stop: {self.update_c}/{self.args.stop_early}]"
+                    f"[Lr: {self.optimizer.param_groups[0]['lr']:4f}][Gamma: {self.args.gamma}][NaN count: {self.nan}]"
                     if self.phase == "Train"
                     else ""
                 )
                 self.logger.info(
-                    f"Epoch: {self.epoch} [{self.phase}][{self.m_dig}]{info_m}[{self.iter * (self.iter_epoch + 1) + self.iter}/{self.rep * dataloader_len}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}, Correlation: {correlation:.2f} micro Precision: {(micro_precision * 100):.2f}%"
+                    f"Epoch: {self.epoch} [{self.phase}][{self.m_dig}]{info_m}[{self.iter}/{dataloader_len}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}, Correlation: {correlation:.2f} micro Precision: {(micro_precision * 100):.2f}%"
                 )
 
                 if self.phase == "Valid":
@@ -190,7 +190,7 @@ class Model(object):
 
             else:
                 self.logger.info(
-                    f"Epoch: {self.epoch} [{self.phase}][{self.m_dig}][{self.iter * (self.iter_epoch + 1) + self.iter}/{self.rep * dataloader_len}][Lr: {self.optimizer.param_groups[0]['lr']:4f}][Early Stop: {self.update_c}/{self.args.stop_early}][{self.m_dig}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}, Correlation: {correlation:.2f}"
+                    f"Epoch: {self.epoch} [{self.phase}][{self.m_dig}][{self.iter}/{dataloader_len}][Lr: {self.optimizer.param_groups[0]['lr']:4f}][Early Stop: {self.update_c}/{self.args.stop_early}][{self.m_dig}] ---- >  loss: {self.train_loss.avg if self.phase == 'Train' else self.val_loss.avg:.04f}, Correlation: {correlation:.2f}"
                 )
                 if self.phase == "Valid":
                     if self.best_loss[self.m_dig] > self.val_loss.avg:
@@ -303,42 +303,41 @@ class Model(object):
         )
         self.prev_model = deepcopy(self.model)
 
-        self.rep = 3
-        for self.iter_epoch in range(self.rep):
-            for self.iter, (img, label, self.img_names, _, _, _) in enumerate(
-                self.train_loader
-            ):
-                img, label = img.to(device), label.to(device)
-                pred = self.model(img)
+        for self.iter, (img, label, self.img_names, _, _, _) in enumerate(
+            self.train_loader
+        ):
+            img, label = img.to(device), label.to(device)
 
-                if self.args.mode == "class":
-                    loss = self.class_loss(pred, label)
-                else:
-                    loss = self.regression(pred, label)
+            pred = self.model(img)
 
-                if not self.iter and not self.epoch:
-                    self.wandb_run.log(
-                        {
-                            "train/image": [
-                                wandb.Image(
-                                    img[i],
-                                    caption=f"GT: {label[i]}, Pred: {pred[i].argmax().item()}, Name: {self.img_names[i]}",
-                                )
-                                for i in range(3)
-                            ]
-                        },
-                        step=self.global_step,
-                    )
+            if self.args.mode == "class":
+                loss = self.class_loss(pred, label)
+            else:
+                loss = self.regression(pred, label)
 
-                self.print_loss(len(self.train_loader))
+            if not self.iter and not self.epoch:
+                self.wandb_run.log(
+                    {
+                        "train/image": [
+                            wandb.Image(
+                                img[i],
+                                caption=f"GT: {label[i]}, Pred: {pred[i].argmax().item()}, Name: {self.img_names[i]}",
+                            )
+                            for i in range(3)
+                        ]
+                    },
+                    step=self.global_step,
+                )
 
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+            self.print_loss(len(self.train_loader))
 
-                self.global_step += 1
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-            self.print_loss(len(self.train_loader), final_flag=True)
+            self.global_step += 1
+
+        self.print_loss(len(self.train_loader), final_flag=True)
 
     def valid(self):
         self.phase = "Valid"
